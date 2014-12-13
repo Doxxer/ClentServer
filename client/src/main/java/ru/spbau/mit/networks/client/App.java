a756740927dfaf1c9f63cb22520124d83b24f787
Timur <doxxerACM@gmail.com>
2014-12-13 19:13:29 +0300

    Client reformat code and moved to java 1.8

--- client/src/main/java/ru/spbau/mit/networks/client/App.java
+++ client/src/main/java/ru/spbau/mit/networks/client/App.java
@@ -2,12 +2,9 @@ package ru.spbau.mit.networks.client;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
